4dd01d9f7793f7c2ef00bec66b6fd8e4b5902ff9
Parshva Shah <shahparshva72@gmail.com>
2019-03-09 12:43:35 +0530

    Initial Commit.

--- Scene Detection/AppDelegate.swift
+++ Scene Detection/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  Scene Detection
 //
-//  Created by Varun Shah on 1/19/19.
+//  Created by Parshva Shah on 1/19/19.
 //  Copyright © 2019 Parshva Shah. All rights reserved.
 //
 
@@ -13,12 +13,39 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    var shortcutItemsToProcess: UIApplicationShortcutItem?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if let shortcutItem = launchOptions?[UIApplication.LaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
+            shortcutItemsToProcess = shortcutItem
+        }
+        
         return true
     }
-
+    
+    /// - Tag: PerformAction
+    func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+        // Alternatively, a shortcut item may be passed in through this delegate method if the app was
+        // still in memory when the Home screen quick action was used. Again, store it for processing.
+        shortcutItemsToProcess = shortcutItem
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        if let shortcutItem = shortcutItemsToProcess {
+            
+            let message = "\(shortcutItem.type) triggered"
+            let alertController = UIAlertController(title: "Quick Action", message: message, preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Close", style: .default, handler: nil))
+            window?.rootViewController?.present(alertController, animated: true, completion: nil)
+            
+            // Reset the shorcut item so it's never processed twice.
+            shortcutItemsToProcess = nil
+        }
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
@@ -31,10 +58,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
--- Scene Detection/ViewController.swift
+++ Scene Detection/ViewController.swift
@@ -9,15 +9,24 @@
 import UIKit
 import Vision
 import CoreML
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var imageBtn: UIButton!
+    @IBOutlet weak var cameraBtn: UIButton!
+    
     var picker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        imageBtn.layer.cornerRadius = 10
+        imageBtn.clipsToBounds = true
+        cameraBtn.layer.cornerRadius = 10
+        cameraBtn.clipsToBounds = true
         picker.delegate = self
     }
 
@@ -27,25 +36,84 @@ class ViewController: UIViewController {
         picker.allowsEditing = false
         present(picker, animated: true, completion: nil)
     }
+    
+    @IBAction func captureImage(_ sender: Any) {
+        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerController.SourceType.camera)){
+            picker.sourceType = .camera
+            picker.cameraCaptureMode = .photo
+            picker.allowsEditing = false
+            picker.modalPresentationStyle = .fullScreen
+            present(picker, animated: true, completion: nil)
+        }
+        else{
+            let alert  = UIAlertController(title: "Warning", message: "You don't have camera", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
 }
 
 extension ViewController: UIImagePickerControllerDelegate,UINavigationControllerDelegate{
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let pickedImage = info[.originalImage] as? UIImage{
+        dismiss(animated: true, completion: nil)
+        /* if let pickedImage = info[.originalImage] as? UIImage{
             imageView.contentMode = .scaleAspectFit
             imageView.image = pickedImage
+        } */
+        
+        guard let pickedImage = info[.originalImage] as? UIImage else {
+            fatalError("couldn't load image from Photos")
         }
         
-        dismiss(animated: true, completion: nil)
+        imageView.image = pickedImage
+        imageView.contentMode = .scaleAspectFit
+        
+        guard let ciImage = CIImage(image: pickedImage) else {
+            fatalError("couldn't convert UIImage to CIImage")
+        }
+        
+        detectScene(image: ciImage)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
 
 extension ViewController{
     func detectScene(image: CIImage){
-        guard let model = try? VNCoreMLModel(for: VGG16().model) else {
+        guard let model = try? VNCoreMLModel(for: SceneDetectionClassifier().model) else {
             fatalError("can't load Places ML model")
         }
         
+        let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+            guard let results = request.results as? [VNClassificationObservation],
+                let topResult = results.first else {
+                    fatalError("unexpected result type from VNCoreMLRequest")
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.resultLbl.text = "It's \(topResult.identifier) with Confidence: \(Int(topResult.confidence * 100))%"
+            }
+            
+            let string = "It's \(topResult.identifier) with Confidence: \(Int(topResult.confidence * 100))%"
+            
+            let utterance = AVSpeechUtterance(string: string)
+            utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+            
+            let synth = AVSpeechSynthesizer()
+            synth.speak(utterance)
+        }
         
+        // Run the Core ML GoogLeNetPlaces classifier on global dispatch queue
+        let handler = VNImageRequestHandler(ciImage: image)
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([request])
+            } catch {
+                print(error)
+            }
+        }
+
     }
 }
